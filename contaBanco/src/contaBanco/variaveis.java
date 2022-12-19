c18513fb6e237fa6c86108e29ecf191356506288
joao <joao.martins@sinqia.com.br>
2022-12-19 18:14:53 -0300

    criado variaveis

---
+++ contaBanco/src/contaBanco/variaveis.java
@@ -0,0 +1,44 @@
+package contaBanco;
+
+import java.math.BigDecimal;
+
+public class variaveis {
+
+	BigDecimal numero;
+	String agencia;
+	String nomeCliente;
+	Float saldo;
+
+	public BigDecimal getNumero() {
+		return numero;
+	}
+
+	public void setNumero(BigDecimal numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public Float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Float saldo) {
+		this.saldo = saldo;
+	}
+	
+}
