ebdf536466505131120f40b63bb21fd8a1eea523
joao <joao.martins@sinqia.com.br>
2022-12-19 17:22:38 -0300

    adicionado TODO

--- contaBanco/src/contaBanco/ContaTerminal.java
+++ contaBanco/src/contaBanco/ContaTerminal.java
@@ -3,7 +3,14 @@ package contaBanco;
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-
+		
+		/**
+		 * TODO:
+		 * conhecer e importar a classe scanner
+		 * Exibir mensagens para nosso usuario
+		 * Obter pelo scanner os valores digitados no terminal 
+		 * exibir mensagem conta criada.
+		 */
 	}
 
 }
